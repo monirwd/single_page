9cffe4e65de5a3699a1afc73a7200c157b7fc51b
monirwd <monirwd007@gmail.com>
2021-11-18 18:10:59 +0600

    hero-area

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,64 @@
-(function($){
-	$(document).ready(function() {	
+(function ($) {
+	$(document).ready(function () {
 
 		// Scroll to Top
-		jQuery('.scrolltotop').click(function(){
-			jQuery('html').animate({'scrollTop' : '0px'}, 400);
+		jQuery('.scrolltotop').click(function () {
+			jQuery('html').animate({ 'scrollTop': '0px' }, 400);
 			return false;
 		});
-		
-		jQuery(window).scroll(function(){
+
+		jQuery(window).scroll(function () {
 			var upto = jQuery(window).scrollTop();
-			if(upto > 500) {
+			if (upto > 500) {
 				jQuery('.scrolltotop').fadeIn();
 			} else {
 				jQuery('.scrolltotop').fadeOut();
 			}
 		});
 
+		//header-fixed
+		$(window).scroll(function () {
+			var scroll = $(window).scrollTop();
 
-		
+			if (scroll >= 15) {
+				$(".header-area").addClass("headerfixed");
+			} else {
+				$(".header-area").removeClass("headerfixed");
+			}
+		});
 
-				
-		
-		
-		
-		
-		
-		
-		
-		
+		//scroll-down
+		$(".hero-left a").click(function () {
+			$('html, body').animate({
+				scrollTop: $(".course-area").offset().top
+			}, 500);
+		});
+
+		//scroll-navigation
+		$('body').scrollspy({ target: '#main-nav' });
+
+		$('#main-nav a').on('click', function (e) {
+			if (this.hash !== '') {
+				e.preventDefault();
+				const hash = this.hash;
+				$('html, body').animate({
+					scrollTop: $(hash).offset().top
+				}, 500, function () {
+					window.location.hash = hash;
+				});
+			}
+		});
+
+
+
+
+
+
+
+
+
+
+
+
 	});
 })(jQuery);
